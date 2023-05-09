2f107c154acf18c4b5bc0d826e97d1d2fb2e8ffe
Akash1298 <akashrana0153@gmail.com>
2023-05-09 15:10:24 +0530

    added sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'blogs#home'
   get 'about', to: 'blogs#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except:[:new]
 end
